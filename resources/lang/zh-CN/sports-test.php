04f452ed1f1b8d597e197aa33aadd44dbe319d11
xiaoel <15927559762@163.com>
2020-12-21 23:37:44 +0800

    增加体测叔叔

---
+++ resources/lang/zh-CN/sports-test.php
@@ -0,0 +1,22 @@
+<?php 
+return [
+    'labels' => [
+        'SportsTest' => 'SportsTest',
+    ],
+    'fields' => [
+        'height' => '身高',
+        'weight' => '体重',
+        'BMI' => 'BMI',
+        'arm_spread' => '臂展',
+        'finger_span' => '手指跨度',
+        'sitting_position' => '坐位体能屈',
+        'jump' => '30s连续跳跃',
+        'sensitive' => '猫区灵敏测试',
+        'toss_ball' => '跪姿抛球',
+        'vertical_jump' => '垂直跳跃',
+        'age' => '年龄',
+        'user_id' => '用户',
+    ],
+    'options' => [
+    ],
+];
---
+++ resources/lang/zh-CN/video.php
@@ -0,0 +1,12 @@
+<?php 
+return [
+    'labels' => [
+        'Video' => 'Video',
+    ],
+    'fields' => [
+        'video_name' => 'video_name',
+        'video_url' => 'video_url',
+    ],
+    'options' => [
+    ],
+];
